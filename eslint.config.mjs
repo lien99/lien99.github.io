4ac2f3a3850e6ba12eb9f5bc799327f3329a883b
xjtulien <xjtuerlien@gmail.com>
2025-04-09 18:44:52 +0800

    github pages setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
-import antfu from '@antfu/eslint-config'
+// import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  unocss: true,
-  typescript: true,
-  astro: true,
+// export default antfu({
+//   unocss: true,
+//   typescript: true,
+//   astro: true,
+// })
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://lien99.github.io',
 })
